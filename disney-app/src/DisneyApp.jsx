804cce4944df65b78b74b699bab19c4a53484551
Ivo Delev <ivo.delev@wp.pl>
2024-04-27 16:45:51 +0200

    adjustments to start

---
+++ disney-app/src/DisneyApp.jsx
@@ -0,0 +1,11 @@
+import "./DisneyApp.css";
+
+function DisneyApp() {
+  return (
+    <>
+      <h1>Disney+</h1>
+    </>
+  );
+}
+
+export default DisneyApp;
--- disney-app/src/main.jsx
+++ disney-app/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import DisneyApp from "./DisneyApp.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DisneyApp />
+  </React.StrictMode>
+);
